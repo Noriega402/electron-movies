02c97671796070cd5fea530e80f6b26576e92ead
Noriega402 <d.noriegachajon@gmail.com>
2023-02-08 00:18:02 -0600

    API fetch wit async await

--- src/renderer.js
+++ src/renderer.js
@@ -1,26 +1,46 @@
 const KEY = "1f3a836c41734431bdc7d1bdb82fdf36";
-const API = `https://api.themoviedb.org/3/movie/100?api_key=${KEY}`;
-const container = document.querySelector('#container');
-const btn = document.querySelector('.btn');
-const btnMenuMobile = document.querySelector('#hamburger');
-const menu = document.querySelector('#menu');
-const menu1 = document.querySelector('#menu-left');
-const menu2 = document.querySelector('#menu-right');
+// const API = `https://api.themoviedb.org/3/movie/100?api_key=${KEY}`;
+const API = `https://api.themoviedb.org/3/movie/popular?api_key=${KEY}&language=en-US&page=1`;
+const IMAGES = "https://image.tmdb.org/t/p/w500";
+const container = document.querySelector('.row');
 
-btnMenuMobile.addEventListener('click', e =>{
-    btnMenuMobile.classList.toggle('is-active');
-    // menu1.classList.toggle('hidden')
-    menu.classList.toggle('hidden');
-    menu1.classList.toggle('hidden');
-    menu2.classList.toggle('hidden');
+window.addEventListener('DOMContentLoaded', async (e) => {
+    // const p = document.createElement('H3');
+    // p.setAttribute('id', 'name');
+    // p.innerText = "Daniel Noriega";
+    // container.append(p);
+    try {
+        const response = await fetch(`${API}`);
+        const status = response.status;
+        if (status === 200) {
+            // console.log(status);
+            const data = await response.json();
+            // console.log(data);
+
+            // let fragment = document.createDocumentFragment();
+            let movies = '';
+            // data.results.forEach(movie => console.log(movie.title));
+            data.results.forEach(item => {
+                // console.log(item);
+                movies += `<div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card mb-3">
+                <img src="${IMAGES}${item.backdrop_path}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${item.title}</h5>
+                    <span class="badge rounded-pill bg-primary">${item.release_date}</span>
+                </div>
+            </div>
+        </div>`
+            });
+
+            container.innerHTML = movies;
+        }else{
+            console.error("hubo un error...");
+        }
+    } catch (err) {
+        console.error(err);
+    }
 })
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     const p = document.createElement('H3');
-//     p.setAttribute('id', 'name');
-//     p.innerText = "Daniel Noriega";
-//     container.append(p);
-// })
 
 // btn.addEventListener('click', async e => {
 //     // console.log("Daniel");
